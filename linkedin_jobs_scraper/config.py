e48bcc5abb72d1a821cf3f8b6a7f6b0f7c4bd9dc
Dimas Satria <dimassatriia@gmail.com>
2021-12-02 20:02:06 +0700

    🐞 Fix bug anonymous mode randomly.

--- linkedin_jobs_scraper/config.py
+++ linkedin_jobs_scraper/config.py
@@ -3,7 +3,10 @@ import logging
 
 
 class Config:
+    os.environ["LI_AT_COOKIE"] = "AQEDATL5DxIFTuPsAAABfXs13I0AAAF9n0JgjVYAU4X6cGNAQ9I5jPwzbgTtL5rabjZxqEp6DuGvFo687mMp62zhM5EXlKKr1f6nPdBify_xjaDQ6YeEb48G-tCqH3WvNTv4BSZ2da9R_yT65Zpw8UPw"
     LI_AT_COOKIE = os.environ['LI_AT_COOKIE'] if 'LI_AT_COOKIE' in os.environ else None
+    print("debug baru")
+    print(os.environ['LI_AT_COOKIE'])
     LOGGER_NAMESPACE = 'li:scraper'
 
     _level = logging.INFO
