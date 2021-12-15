489e4bc6592791392f1ee6fdbf40c4c329f796e1
Dimas Satria <dimassatriia@gmail.com>
2021-12-16 00:09:09 +0700

    ✨ Fitur ubah data cv via input text.

--- myapp.py
+++ myapp.py
@@ -1,3 +1,7 @@
+from tempfile import NamedTemporaryFile
+import csv
+import shutil
+
 import streamlit as st
 import os
 
@@ -60,6 +64,9 @@ if 'isUser' not in st.session_state:
 
 if 'isAdmin' not in st.session_state:
     st.session_state['isAdmin'] = False
+
+if 'isCvDataUpdated' not in st.session_state:
+    st.session_state['isCvDataUpdated'] = False
 
 # variable contains all countries name in the world
 countries_name = ["Afghanistan", "Albania", "Algeria", "American Samoa", "Andorra", "Angola", "Anguilla", "Antarctica", "Antigua and Barbuda", "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia", "Bosnia and Herzegowina", "Botswana", "Bouvet Island", "Brazil", "British Indian Ocean Territory", "Brunei Darussalam", "Bulgaria", "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Cayman Islands", "Central African Republic", "Chad", "Chile", "China", "Christmas Island", "Cocos (Keeling) Islands", "Colombia", "Comoros", "Congo", "Congo, the Democratic Republic of the", "Cook Islands", "Costa Rica", "Cote d'Ivoire", "Croatia (Hrvatska)", "Cuba", "Cyprus", "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "East Timor", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Falkland Islands (Malvinas)", "Faroe Islands", "Fiji", "Finland", "France", "France Metropolitan", "French Guiana", "French Polynesia", "French Southern Territories", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece", "Greenland", "Grenada", "Guadeloupe", "Guam", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Heard and Mc Donald Islands", "Holy See (Vatican City State)", "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", "Iran (Islamic Republic of)", "Iraq", "Ireland", "Israel", "Italy", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Korea, Democratic People's Republic of", "Korea, Republic of", "Kuwait", "Kyrgyzstan", "Lao, People's Democratic Republic", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libyan Arab Jamahiriya", "Liechtenstein", "Lithuania", "Luxembourg", "Macau", "Macedonia, The Former Yugoslav Republic of", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Martinique", "Mauritania", "Mauritius", "Mayotte", "Mexico", "Micronesia, Federated States of", "Moldova, Republic of", "Monaco", "Mongolia", "Montserrat", "Morocco", "Mozambique", "Myanmar", "Namibia", "Nauru", "Nepal", "Netherlands", "Netherlands Antilles", "New Caledonia", "New Zealand", "Nicaragua", "Niger", "Nigeria", "Niue", "Norfolk Island", "Northern Mariana Islands", "Norway", "Oman", "Pakistan", "Palau", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Pitcairn", "Poland", "Portugal", "Puerto Rico", "Qatar", "Reunion", "Romania", "Russian Federation", "Rwanda", "Saint Kitts and Nevis", "Saint Lucia", "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia", "Senegal", "Seychelles", "Sierra Leone", "Singapore", "Slovakia (Slovak Republic)", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Georgia and the South Sandwich Islands", "Spain", "Sri Lanka", "St. Helena", "St. Pierre and Miquelon", "Sudan", "Suriname", "Svalbard and Jan Mayen Islands", "Swaziland", "Sweden", "Switzerland", "Syrian Arab Republic", "Taiwan, Province of China", "Tajikistan", "Tanzania, United Republic of", "Thailand", "Togo", "Tokelau", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan", "Turks and Caicos Islands", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States", "United States Minor Outlying Islands", "Uruguay", "Uzbekistan", "Vanuatu", "Venezuela", "Vietnam", "Virgin Islands (British)", "Virgin Islands (U.S.)", "Wallis and Futuna Islands", "Western Sahara", "Yemen", "Yugoslavia", "Zambia", "Zimbabwe"]
@@ -165,6 +172,7 @@ def main():
 	choice = st.sidebar.selectbox("Menu", menu)
 
 	isSearched = st.session_state.isSearched
+	isCvDataUpdated = st.session_state.isCvDataUpdated
 	
 	if choice == "Home":
 		st.title("Home")
@@ -741,19 +749,56 @@ def main():
 					st.error("Please update the ad first.")
 
 			elif task == "CV Template":
-				st.subheader("Download Template CV ")
-				st.write("Click the button to download CV template")
-				# st.write('https://drive.google.com/file/d/1LUyxJgdXEQdPMTuqSCdNjdIKqVjE7z80/view?usp=sharing')
-				with open("templatecv.csv", "rb") as file:
-					btn = st.download_button(
-						label="Download",
-						data=file,
-						file_name="template.csv",
-						mime="text/csv",
-					)
-				st.subheader("CV Writing Guide")
-				image = Image.open("panduanedit.png")
-				st.image(image, caption="Format CV")
+				st.subheader("Update CV Data")		
+				education_value ="2017-Victoria University, Statistics, PhD in Statistics 2014-Victoria University, Computational Mathematics, Master of Science 2012-Melbourne University, Computer Science, Bachelor of Science If you're a recent graduate and you don't have relevant job experience or no experience at all, you should list what you have accomplished during your education."		
+				education_section = st.text_area('Education Section:',education_value,200)
+						
+				experience_value ="Refined personalization algorithms for 1.5M+ customers on web and mobile, boosting engagement and time spent on the platform by 25%. Increased team efficiency by 20% and reduced the company's costs by 27% by solving complex business problems using Machine Learning techniques like Regression, Classification, Supervised and Unsupervised Recommenders. Increased the company's revenue by 15% by locating and defining 2 new process improvement opportunities. Worked closely with management to prioritize reporting needs, provided analysis and deep insights around critical indicators of the company performance, like volume, gross profit, and warehouse efficiency."		
+				experience_section = st.text_area('Experience Section:',experience_value,200)
+
+				skill_value ="Machine Learning, Data Mining, Data Management, Big Data, Quantitative Analysis, Visualization Tools, Python, TensorFlow ,PyTorch ,SpaCy ,NLTK ,SQL ,Hadoop ,Apache ,Spark ,Excel, SAS, MatLab, R"
+				skill_section = st.text_area('Skill Section:',skill_value,200)
+
+				if st.button("Update Data"):
+					cv_desc_value = "Education Section: " + education_section + " " + "Experience Section: " + experience_section + " " + "Skills Section: " + skill_section
+					st.warning(cv_desc_value)
+					
+					st.success("CV Data Updated!")
+					st.session_state.isCvDataUpdated = True
+					isCvDataUpdated = st.session_state.isCvDataUpdated	
+
+					cv_desc_value = "Education Section: " + education_section + " " + "Experience Section: " + experience_section + " " + "Skills Section: " + skill_section
+					# csv header
+					fieldnames = ['cv_desc']
+
+					# csv data
+					rows = [
+						{'cv_desc': cv_desc_value}
+					]
+
+					with open('templatecv.csv', 'w', encoding='UTF8', newline='') as f:
+						writer = csv.DictWriter(f, fieldnames=fieldnames)
+						writer.writeheader()
+						writer.writerows(rows)
+										
+				
+				st.subheader("Download Template CV ")		
+				if isCvDataUpdated == True:		
+					st.write("Click the button to download CV template")	
+
+					with open("templatecv.csv", "rb") as file:
+						
+						st.download_button(
+							label="Download",
+							data=file,
+							file_name="template.csv",
+							mime="text/csv",
+						)				
+					st.subheader("CV Writing Guide")
+					image = Image.open("panduanedit.png")
+					st.image(image, caption="Format CV")
+				else:
+					st.error("Please update CV data first.")
 
 		else:
 			st.warning("Incorrect Username/Password")			
